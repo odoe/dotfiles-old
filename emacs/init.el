605876b41dcd2ef87539c37a91d6ab270577155a
odoe <odoe@odoe.net>
2014-04-15 21:40:02 -0700

    new snippets, config updates

--- emacs/init.el
+++ emacs/init.el
@@ -2,3 +2,13 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/")
 (package-initialize))
+
+(unless (package-installed-p 'clojure-mode)
+  (package-refresh-contents)
+  (package-install 'clojure-mode))
+
+(unless (package-installed-p 'cider)
+  (package-install 'cider))
+
+;; (require 'paredit) if you didn't install it via package.el)
+(add-hook 'clojure-mode-hook 'paredit-mode)
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,12 @@
 set GOROOT /usr/local/go
 set -x PATH $PATH $GOROOT/bin
 set -x PATH $PATH ~/bin/elixir/bin
+set -x PATH $PATH ~/bin
+
+function fish_prompt
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+  echo -n ' > '
+end
+
