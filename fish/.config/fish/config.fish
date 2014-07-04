3842f2642e8c49197b631b87abe69db14fb1d679
odoe <odoe@odoe.net>
2014-07-03 19:18:41 -0700

    new updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,19 @@
-set GOROOT /usr/local/go
+set GOROOT ~/bin/go
 set -x PATH $PATH $GOROOT/bin
 set -x PATH $PATH ~/bin/elixir/bin
 set -x PATH $PATH ~/bin
+set -x PATH $PATH ~/bin/go-ide/bin
+
+alias g="git"
+alias gl='git pull'
+alias gp='git push'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 
 function fish_prompt
   set_color $fish_color_cwd
