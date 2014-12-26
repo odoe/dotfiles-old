de4c2c3b0c6d7cb0b9d6cd8bfbf683d2a204cf9f
Rene Rubalcava <odoe@odoe.net>
2014-12-25 18:53:37 -0800

    updated configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,13 @@
 set GOROOT ~/bin/go
+set SCALA_HOME ~/bin/scala
 set -x PATH $PATH $GOROOT/bin
+set -x PATH $PATH $SCALA_HOME/bin
 set -x PATH $PATH ~/bin/elixir/bin
 set -x PATH $PATH ~/bin
 set -x PATH $PATH ~/bin/go-ide/bin
+set -x PATH $PATH ~/bin/eclipse
+set -x PATH $PATH ~/local/bin
+set -x PATH $PATH ~/.cabal/bin
 
 alias g="git"
 alias gl='git pull'
